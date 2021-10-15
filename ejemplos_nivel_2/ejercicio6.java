c47d1d3ca3da28927039d7e141178cbead934a8c
CarlosRudaz <carlos_rudaz@hotmail.com>
2021-10-14 21:16:01 -0300

    Cargando ejercicios 6 y 7 de complementarios2

--- ejemplos_nivel_2/ejercicio6.java
+++ ejemplos_nivel_2/ejercicio6.java
@@ -1,7 +1,61 @@
 package ejemplos_nivel_2;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class ejercicio6 {
     public static void main(String[] args) {
-        
+        /* Se dispone de una colección de Empleados, de cada empleado se conoce:
+        ○Nombre y Apellido
+        ○DNI
+        ○horasTrabajadas
+        ○valorPorHoraTodos 
+        los empleados están cargados en un Set (HashSet), se desea calcular el sueldo (horasTrabajadas 
+        x valorPorHora) de toda esa lista para luego almacenar en un Map (o Diccionario) donde la 
+        clave (key) es el dni y el valor(value) es el sueldo calculado. */
+
+        HashSet<Employee> empleado = new HashSet<>();
+        HashMap<Integer, Float> salario = new HashMap<>();
+
+        empleado.add(new Employee("Carlos Rudaz", 34388697, 8, 150));
+        empleado.add(new Employee("Ana Maria Ramirez", 36752189, 7, 180));
+        empleado.add(new Employee("Humberto Rodriguez", 30125849, 5, 230));
+        empleado.add(new Employee("Gladys Lastape", 14069215, 5, 500));
+        empleado.add(new Employee("Roberto Gomez Bolaneo", 7526325, 5, 200));
+
+        System.out.println("Calculo del salario de los empleados");
+
+        for (Employee lista : empleado) {
+            System.out.println(lista.nombre + " - DNI " + lista.dni + " - Horas de trabajo: " + lista.horasTrabajadas + " - Valor por hora: " + lista.valorPorHora);
+        }
+
+        for (Employee crearUnaTabla : empleado) {
+            salario.put(crearUnaTabla.clave(), crearUnaTabla.valor());
+        }
+        System.out.println("\nSalario percibido:\n");
+        salario.forEach((dni, valor) -> System.out.println("DNI: " + dni + " - Salario: " + valor));
+    }
+}
+    
+class Employee {
+    String nombre;
+    int dni;
+    float horasTrabajadas;
+    float valorPorHora;
+
+
+    public Employee(String nombre, int dni, float horasTrabajadas, float valorPorHora) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.horasTrabajadas = horasTrabajadas;
+        this.valorPorHora = valorPorHora;
+    }
+
+    public int clave() {
+        return this.dni;
+    }
+
+    public float valor() {
+        return this.horasTrabajadas * this.valorPorHora;
     }
 }
--- ejemplos_nivel_2/ejercicio7.java
+++ ejemplos_nivel_2/ejercicio7.java
@@ -1,7 +1,46 @@
 package ejemplos_nivel_2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ejercicio7 {
     public static void main(String[] args) {
-        
+        /* Crear una función que dado 2 argumentos (int, siendo el primero menor alsegundo), 
+        nos devuelva un array de Strings. Con la secuencia de númerosenteros de principio a final. 
+        Pero si el número es multiplo de 2 colocara el valor“Fizz”, si es múltiplo de 3 “Buzz” y si 
+        es a la vez múltiplo de ambos colocara“FizzBuzz”.Observacion: Los 2 argumentos indican con que 
+        valor se arranca a calcular y elsegundo con qué valor debe frenar (no se incluye en el cálculo)
+        Ejemplo: (1, 5) ----> calculará valores de 1, 2, 3, 4  */
+
+        int num1;
+        int num2;
+        ArrayList<String> sumaTotal = new ArrayList<String>();
+        Scanner consola = new Scanner(System.in);
+
+        System.out.println("Ingrese su primer numero, que sea del menor: ");
+        num1 = consola.nextInt();
+        consola.nextLine();
+        System.out.println("Ingrese su segundo numero, el más grande: ");
+        num2 = consola.nextInt();
+        consola.nextLine();
+
+        for (int i = num1; i < num2; i++) {
+            if (num1 % 2 == 0 && num1 % 3 == 0) {
+                sumaTotal.add(i + ". FizzBuzz");
+            } else if (num1 % 3 == 0) {
+                sumaTotal.add(i + ". Buzz");
+            } else if (num1 % 2 == 0) {
+                sumaTotal.add(i + ". Fizz");
+            } else {
+                System.out.println("Numero no multiplo de 2 ni de 3");
+            }
+
+        }
+        for (int j = 0; j < sumaTotal.size(); j++) {
+            System.out.println(sumaTotal.get(j));
+
+        }
+
+        consola.close();
     }
 }
